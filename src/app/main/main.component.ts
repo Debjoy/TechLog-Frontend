1a018440666ccb370371d23ea38c63d8e772264a
Debjoy <debjoybhowal7@gmail.com>
2020-05-02 21:30:07 +0530

    made the website responsive

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,50 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { CookieService } from 'ngx-cookie-service';
-import { Router } from '@angular/router';
-import { LoginService } from '../login.service';
-import { NgxSpinnerService } from 'ngx-spinner';
-import { ToastrService } from 'ngx-toastr';
-
+import { Component, OnInit } from "@angular/core";
+import { CookieService } from "ngx-cookie-service";
+import { Router } from "@angular/router";
+import { LoginService } from "../login.service";
+import { NgxSpinnerService } from "ngx-spinner";
+import { ToastrService } from "ngx-toastr";
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  selector: "app-main",
+  templateUrl: "./main.component.html",
+  styleUrls: ["./main.component.css"],
 })
 export class MainComponent implements OnInit {
-
   private cookieToken: any;
   private cookieUsername: any;
+  private search_expand = false;
 
-  constructor(private cookieService: CookieService, private router: Router, private loginService:LoginService,private spinner: NgxSpinnerService,private toastr:ToastrService) { }
+  constructor(
+    private cookieService: CookieService,
+    private router: Router,
+    private loginService: LoginService,
+    private spinner: NgxSpinnerService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit() {
-    this.cookieToken = this.cookieService.get('token');
-    this.cookieUsername = this.cookieService.get('user');
+    this.cookieToken = this.cookieService.get("token");
+    this.cookieUsername = this.cookieService.get("user");
     let LoginData: any;
     this.spinner.show("spinner1");
     LoginData = {
-      "username": this.cookieUsername ,
-      "token": this.cookieToken
-    }
-    if(this.cookieToken == ''){
-      this.router.navigate(['login']);
-    }else{
-      this.loginService.existsByUsernameAndToken(LoginData).subscribe(res=>{
-        if(res != true){
-          this.cookieService.set('token','');
-          this.cookieService.set('user','');
-          this.router.navigate(['login']);
-        }else{
+      username: this.cookieUsername,
+      token: this.cookieToken,
+    };
+    if (this.cookieToken == "") {
+      this.router.navigate(["login"]);
+    } else {
+      this.loginService.existsByUsernameAndToken(LoginData).subscribe((res) => {
+        if (res != true) {
+          this.cookieService.set("token", "");
+          this.cookieService.set("user", "");
+          this.router.navigate(["login"]);
+        } else {
           console.log("Logged in Successfully");
-          this.toastr.success('Logged in Successfully', 'Awesome!',{
-            positionClass:'toast-top-right'
+          this.toastr.success("Logged in Successfully", "Awesome!", {
+            positionClass: "toast-top-right",
           });
           this.spinner.hide("spinner1");
         }
-      }
-      );
+      });
     }
   }
-
 }
