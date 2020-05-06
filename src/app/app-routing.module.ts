0859ffb570d75fb80dfa0d934f16a132ed3b9593
Debjoy <debjoybhowal7@gmail.com>
2020-05-06 20:18:51 +0530

    profile loading with url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,34 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './main/home/home.component';
-import { ProfileComponent } from './main/profile/profile.component';
-import { UserPostComponent } from './main/user-post/user-post.component';
-import { PostComponent } from './main/post/post.component';
-import { MainComponent } from './main/main.component';
-import { LoginComponent } from './login/login.component';
-import { EditpostComponent } from './main/editpost/editpost.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./main/home/home.component";
+import { ProfileComponent } from "./main/profile/profile.component";
+import { UserPostComponent } from "./main/user-post/user-post.component";
+import { PostComponent } from "./main/post/post.component";
+import { MainComponent } from "./main/main.component";
+import { LoginComponent } from "./login/login.component";
+import { EditpostComponent } from "./main/editpost/editpost.component";
 
 const routes: Routes = [
-    { path: '', component: MainComponent,
-    children:[
-        { path: 'home' , component: HomeComponent},
-        { path: 'profile' , component: ProfileComponent},
-        { path: 'user-post' , component: UserPostComponent},
-        {path:'edit/:id',component: EditpostComponent},
-        {path:'edit', redirectTo: ''},
-        {path:'post/:id',component: PostComponent},
-        {path:'post', redirectTo: ''},
-        { path: '',component: HomeComponent}
-        
-    ]},
-    { path: 'login', component: LoginComponent}
+  {
+    path: "",
+    component: MainComponent,
+    children: [
+      { path: "home", component: HomeComponent },
+      { path: "profile/:id", component: ProfileComponent },
+      { path: "profile", component: ProfileComponent },
+      { path: "user-post", component: UserPostComponent },
+      { path: "edit/:id", component: EditpostComponent },
+      { path: "edit", redirectTo: "" },
+      { path: "post/:id", component: PostComponent },
+      { path: "post", redirectTo: "" },
+      { path: "", component: HomeComponent },
+    ],
+  },
+  { path: "login", component: LoginComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/main/profile/profile.component.ts
+++ src/app/main/profile/profile.component.ts
@@ -1,136 +1,171 @@
-import { Component, OnInit } from '@angular/core';
-import { LoginService } from '../../login.service';
-import { CookieService } from 'ngx-cookie-service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { switchMap, distinctUntilChanged, debounceTime } from 'rxjs/internal/operators';
-import { PostService } from '../post.service';
-import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
+import { Component, OnInit } from "@angular/core";
+import { LoginService } from "../../login.service";
+import { CookieService } from "ngx-cookie-service";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { PostService } from "../post.service";
+import { Router, ActivatedRoute } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { parseTemplate } from "@angular/compiler";
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.css"],
 })
 export class ProfileComponent implements OnInit {
+  username: any;
+  title: any;
+  editProfileModal = 0;
+  editPasswordModal = 0;
 
-  username: any;  
-  title:any;
-  editProfileModal=0;
-  editPasswordModal=0;
+  editNameLoading = 0;
+  editUsernameLoading = 0;
 
-  editNameLoading=0;
-  editUsernameLoading=0;
+  editPasswordLoading = 0;
 
-  editPasswordLoading=0;
+  form1: any;
+  form2: any;
+  form3: any;
 
-  form1 : any;
-    form2 :any;
-    form3:any;
-  
-  constructor(private router:Router,private loginService: LoginService,private postService:PostService,private cookieService:CookieService, private toastr: ToastrService) { }
-   userDetails:any
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private postService: PostService,
+    private cookieService: CookieService,
+    private toastr: ToastrService,
+    private route: ActivatedRoute
+  ) {}
+  userDetails: any;
+  cookieUserName: any;
+  foreignUser = true;
+  loadUser: any;
   ngOnInit() {
     //this.getResult();
-    
-    var user1:any={
-      username:this.cookieService.get('user')
-    }
-  this.loginService.findByUsername(this.cookieService.get('user'))
-  .subscribe(
-   res=>{//this.user=res,
-    this.userDetails=res[0];
-    this.form1=new FormGroup({
-        rname: new FormControl (this.userDetails.name, [Validators.required])})
-    this.form3=new FormGroup({
-          rpassword:new FormControl(this.userDetails.password,[Validators.required,
-            Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d@$!%*#?&!^]{8,}$')]),
-          rcpassword: new FormControl(this.userDetails.password, [Validators.required])
-          }, this.pwdMatchValidator);
+    this.cookieUserName = this.cookieService.get("user");
 
-          
- },
-   err=>console.error(err)
-);
-  }
-  
-  pwdMatchValidator(frm: FormGroup){
-    return frm.get('rpassword').value === frm.get('rcpassword').value ? null : {'mismatch': true};
+    this.route.paramMap.subscribe((param) => {
+      if (param.has("id")) {
+        let paramUser = param.get("id");
+        if (paramUser === this.cookieUserName) {
+          this.loadUser = this.cookieUserName;
+          this.foreignUser = false;
+        } else {
+          this.loadUser = paramUser;
+          this.foreignUser = true;
+        }
+      } else {
+        console.log("inside");
+        this.loadUser = this.cookieUserName;
+        this.foreignUser = false;
+      }
+
+      this.loginService.findByUsername(this.loadUser).subscribe(
+        (res) => {
+          //this.user=res,
+          if (res.length < 1) {
+            this.router.navigate(["profile"]);
+            return;
+          }
+          this.userDetails = res[0];
+          this.form1 = new FormGroup({
+            rname: new FormControl(this.userDetails.name, [
+              Validators.required,
+            ]),
+          });
+          this.form3 = new FormGroup(
+            {
+              rpassword: new FormControl(this.userDetails.password, [
+                Validators.required,
+                Validators.pattern(
+                  "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d@$!%*#?&!^]{8,}$"
+                ),
+              ]),
+              rcpassword: new FormControl(this.userDetails.password, [
+                Validators.required,
+              ]),
+            },
+            this.pwdMatchValidator
+          );
+        },
+        (err) => console.error(err)
+      );
+    });
   }
 
-  getResult(){
-    this.loginService.getResult().subscribe(res => {
-      this.username= this.cookieService.get('user');
+  pwdMatchValidator(frm: FormGroup) {
+    return frm.get("rpassword").value === frm.get("rcpassword").value
+      ? null
+      : { mismatch: true };
+  }
+
+  getResult() {
+    this.loginService.getResult().subscribe((res) => {
+      this.username = this.cookieService.get("user");
       //console.log(res.employee_name);
     });
   }
 
-  updateName()
-  {
-    this.editNameLoading=1;
-    let nameEmail={
-    name:this.form1.value.rname,
-    email:this.userDetails.email
-  }
+  updateName() {
+    this.editNameLoading = 1;
+    let nameEmail = {
+      name: this.form1.value.rname,
+      email: this.userDetails.email,
+    };
     this.loginService.updateNameByEmail(nameEmail).subscribe(
-      res=>{
-        this.userDetails.name=this.form1.value.rname;
-        this.editProfileModal=0;
-        this.editNameLoading=0;
-        this.toastr.success('Name Updated', 'Awesome!',{
-          positionClass:'toast-top-center'
+      (res) => {
+        this.userDetails.name = this.form1.value.rname;
+        this.editProfileModal = 0;
+        this.editNameLoading = 0;
+        this.toastr.success("Name Updated", "Awesome!", {
+          positionClass: "toast-top-center",
         });
       },
-      err=>console.error(err))
-  }
-  
-
-  updatePassword()
-  {
-    this.editPasswordLoading=1;
-    let userPass={
-      password:this.form3.value.rpassword,
-      email:this.userDetails.email
-    }
-      this.loginService.updatePasswordByEmail(userPass).subscribe(
-        res=>{
-          console.log(res);
-          this.userDetails.password=this.form3.value.rpassword;
-          this.editPasswordModal=0;
-          this.editPasswordLoading=0;
-          this.toastr.success('Password Updated', 'Awesome!',{
-            positionClass:'toast-top-center'
-          });
-        },
-        err=>console.error(err))
+      (err) => console.error(err)
+    );
   }
 
-  createPost()
-  {
-  let  post={
-      image:"",
-      likes:0,
-      text:"",
-      timestamp:new Date().getTime(),
-      title:this.title,
-      username:this.userDetails.username
-    }
+  updatePassword() {
+    this.editPasswordLoading = 1;
+    let userPass = {
+      password: this.form3.value.rpassword,
+      email: this.userDetails.email,
+    };
+    this.loginService.updatePasswordByEmail(userPass).subscribe(
+      (res) => {
+        console.log(res);
+        this.userDetails.password = this.form3.value.rpassword;
+        this.editPasswordModal = 0;
+        this.editPasswordLoading = 0;
+        this.toastr.success("Password Updated", "Awesome!", {
+          positionClass: "toast-top-center",
+        });
+      },
+      (err) => console.error(err)
+    );
+  }
 
-  this.postService.createPosts(post).subscribe(
-    res=>{
-      console.log(res);
-      this.router.navigate(['edit',res]);
-      this.toastr.success('New Post Created', 'Awesome!',{
-        positionClass:'toast-top-center'
-      });
-    },
-    err=>console.error(err)
+  createPost() {
+    let post = {
+      image: "",
+      likes: 0,
+      text: "",
+      timestamp: new Date().getTime(),
+      title: this.title,
+      username: this.userDetails.username,
+    };
 
-  )
+    this.postService.createPosts(post).subscribe(
+      (res) => {
+        console.log(res);
+        this.router.navigate(["edit", res]);
+        this.toastr.success("New Post Created", "Awesome!", {
+          positionClass: "toast-top-center",
+        });
+      },
+      (err) => console.error(err)
+    );
 
-  
-  /*this.postService.getPostById(2).subscribe(
+    /*this.postService.getPostById(2).subscribe(
     res=>console.log(res)
   )*/
   }
-
 }
