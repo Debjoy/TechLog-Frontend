01683869e083599cef2ceff7d62f9d28218e6228
Debjoy <debjoybhowal7@gmail.com>
2020-06-09 21:45:31 +0530

    Started Working on the Avatar Picker

--- src/app/main/profile/profile.component.ts
+++ src/app/main/profile/profile.component.ts
@@ -16,6 +16,7 @@ export class ProfileComponent implements OnInit {
   title: any;
   editProfileModal = 0;
   editPasswordModal = 0;
+  editProfilePic = 0;
 
   editNameLoading = 0;
   editUsernameLoading = 0;
@@ -25,6 +26,10 @@ export class ProfileComponent implements OnInit {
   form1: any;
   form2: any;
   form3: any;
+
+  selectedImg: string;
+  userImg: string;
+  allImg: string[];
 
   constructor(
     private router: Router,
@@ -41,6 +46,92 @@ export class ProfileComponent implements OnInit {
   postData: any;
   ngOnInit() {
     //this.getResult();
+    this.allImg = [
+      "01.png",
+      "02.png",
+      "03.png",
+      "04.png",
+      "05.png",
+      "06.png",
+      "07.png",
+      "08.png",
+      "09.png",
+      "10.png",
+      "11.png",
+      "12.png",
+      "13.png",
+      "14.png",
+      "15.png",
+      "16.png",
+      "17.png",
+      "18.png",
+      "19.png",
+      "20.png",
+      "21.png",
+      "22.png",
+      "23.png",
+      "24.png",
+      "25.png",
+      "26.png",
+      "27.png",
+      "28.png",
+      "29.png",
+      "30.png",
+      "31.png",
+      "32.png",
+      "33.png",
+      "34.png",
+      "35.png",
+      "36.png",
+      "37.png",
+      "38.png",
+      "39.png",
+      "40.png",
+      "41.png",
+      "42.png",
+      "43.png",
+      "44.png",
+      "45.png",
+      "46.png",
+      "47.png",
+      "48.png",
+      "49.png",
+      "50.png",
+      "51.png",
+      "52.png",
+      "53.png",
+      "54.png",
+      "55.png",
+      "56.png",
+      "57.png",
+      "58.png",
+      "59.png",
+      "60.png",
+      "61.png",
+      "62.png",
+      "63.png",
+      "64.png",
+      "65.png",
+      "66.png",
+      "67.png",
+      "68.png",
+      "69.png",
+      "70.png",
+      "71.png",
+      "72.png",
+      "73.png",
+      "74.png",
+      "75.png",
+      "76.png",
+      "77.png",
+      "78.png",
+      "79.png",
+      "80.png",
+      "81.png",
+    ];
+    this.userImg = this.allImg[0];
+    this.selectedImg = this.userImg;
+
     this.cookieUserName = this.cookieService.get("user");
 
     this.route.paramMap.subscribe((param) => {
@@ -53,10 +144,9 @@ export class ProfileComponent implements OnInit {
           this.loadUser = paramUser;
           this.foreignUser = true;
           //load foreign user's post
-          this.postService.getAllByUsername(this.loadUser)
-          .subscribe(
-            res=>{this.postData=res;}
-          )
+          this.postService.getAllByUsername(this.loadUser).subscribe((res) => {
+            this.postData = res;
+          });
         }
       } else {
         console.log("inside");
@@ -173,5 +263,17 @@ export class ProfileComponent implements OnInit {
     /*this.postService.getPostById(2).subscribe(
     res=>console.log(res)
   )*/
+  }
+
+  openImagePickerModal() {
+    this.selectedImg = this.userImg;
+    this.editProfilePic = 1;
+  }
+  saveImagePickerImg() {
+    console.log(this.selectedImg);
+
+    //after transation is over
+
+    /*this.editProfilePic = 1;*/
   }
 }
